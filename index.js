ebead930f729cc9fc0572a510244dda6495701eb
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2022-12-21 17:15:45 +0530

    Updated

--- index.js
+++ index.js
@@ -16,27 +16,9 @@ const url = "mongodb+srv://darshboyat:3277426269@cluster1.onjgrzp.mongodb.net/?r
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log('Database Connected Successfully....')).catch(()=> console.log('Ohhhh an error occured'))
 
 app.use(bodyParser.json())
-app.use(cors())
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://fatoura-three.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
+app.use(cors({
+    origin: "https://fatoura-five.vercel.app"
+}))
 
 app.use(User)
 app.use(invoice)

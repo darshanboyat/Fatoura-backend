6082ad99af39a88adbe9d07c885ee2287a145328
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2022-12-21 15:51:05 +0530

    Updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://fatoura-three.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'http://fatoura-three.vercel.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
